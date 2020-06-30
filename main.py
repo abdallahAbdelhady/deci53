36983890b8caf4fd6d44b0a7742bd5e44d10f953
abdallahAbdelhady <45328571+abdallahAbdelhady@users.noreply.github.com>
2020-06-30 19:42:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+#!/usr/bin/env python3
+import kivy
+kivy.require('1.0.7')
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+
+class TestApp(App):
+
+    def build(self):
+        # return a Button() as a root widget
+        return Button(text='hello world')
+
+
+if __name__ == '__main__':
+    TestApp().run()
